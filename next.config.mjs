df14723afbd10a5af064e53c64a2ab87be234550
Israel Valderrama <isvalgar@gmail.com>
2026-05-11 09:55:02 +0200

    Deploy Next app to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 import path from "node:path";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const repoName = "Bakana";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: path.resolve(".")
+  output: "export",
+  outputFileTracingRoot: path.resolve("."),
+  basePath: isGithubPages ? `/${repoName}` : undefined,
+  assetPrefix: isGithubPages ? `/${repoName}/` : undefined,
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 };
 
 export default nextConfig;
