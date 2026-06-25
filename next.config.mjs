5b02c5668300aeed83edf3c96bcd484d1a60dcdc
Israel Valderrama <isvalgar@gmail.com>
2026-06-25 12:15:06 +0200

    Remove GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 import path from "node:path";
 
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const repoName = "Bakana";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: isGithubPages ? "export" : undefined,
-  pageExtensions: isGithubPages ? ["tsx", "jsx"] : ["tsx", "ts", "jsx", "js"],
   outputFileTracingRoot: path.resolve("."),
-  basePath: isGithubPages ? `/${repoName}` : undefined,
-  assetPrefix: isGithubPages ? `/${repoName}/` : undefined,
   images: {
     unoptimized: true
   },
